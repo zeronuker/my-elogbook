e2236839b0963dfe9811c83daf53ab244b8b1201
unknown <zero.nuker@gmail.com>
2026-05-07 10:49:37 +0800

    fix column widths to fit screen

--- src/elogbook_2026_v5_1.jsx
+++ src/elogbook_2026_v5_1.jsx
@@ -290,10 +290,8 @@ export default function ELogbook2026() {
                     <span style={{ display: "block" }}>CAPACITY</span>
                   </th>
 
-                  {/* SECTORS group — spans DEPARTURE + ARRIVAL */}
-                  <th colSpan={2} style={{ ...thStyle, borderBottom: "1px solid #1a3050", textAlign: "center", color: "#4fc3f7", fontSize: 9, letterSpacing: "0.15em" }}>
-                    SECTORS
-                  </th>
+                  {/* SECTORS rowspan */}
+                  <th rowSpan={2} style={thStyle}>SECTORS</th>
 
                   {/* DEPARTURE rowspan */}
                   <th rowSpan={2} style={thStyle}>DEPARTURE</th>
@@ -320,9 +318,6 @@ export default function ELogbook2026() {
                   {/* AIRCRAFT sub */}
                   <th style={thSubStyle}>TYPE</th>
                   <th style={thSubStyle}>MARKINGS</th>
-                  {/* SECTORS sub */}
-                  <th style={thSubStyle}>DEPARTURE</th>
-                  <th style={thSubStyle}>ARRIVALS</th>                  
                   {/* DAY sub */}
                   <th style={{ ...thSubStyle, color: "#c8a800" }}>P1</th>
                   <th style={{ ...thSubStyle, color: "#c8a800" }}>P1 U/S</th>
